814922493b26d46888ede978316f9d9147aabe90
Benny Neugebauer <bn@bennyn.de>
2017-05-12 13:53:50 +0200

    const for webpack config

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,7 @@ const webpack = require('webpack');
 // Initializations
 const tsConfig = require('./tsconfig.json');
 const tsProject = gulpTypeScript.createProject('tsconfig.json');
+const webpackConfig = require('./webpack.config.js');
 
 // Aliases
 gulp.task('b', ['build']);
@@ -20,10 +21,12 @@ gulp.task('d', ['dist_ts']);
 gulp.task('i', ['install_bower_assets']);
 gulp.task('w', ['webpack']);
 
-gulp.task('clean', ['clean_commonjs'], () => {
+gulp.task('clean', ['clean_commonjs', 'clean_window'], () => {
 });
 
 gulp.task('clean_commonjs', () => gulp.src('dist/commonjs').pipe(gulpClean()));
+
+gulp.task('clean_window', () => gulp.src('dist/window').pipe(gulpClean()));
 
 gulp.task('install_bower', () => gulpBower({cmd: 'install'}));
 
@@ -62,7 +65,7 @@ gulp.task('default', ['build'], function() {
 });
 
 gulp.task('webpack', ['dist_ts'], function(callback) {
-  const compiler = webpack(require('./webpack.config.js'));
+  const compiler = webpack(webpackConfig);
 
   compiler.apply(new ProgressPlugin((percentage, message) => {
     console.log(`${~~(percentage * 100)}%`, message);
