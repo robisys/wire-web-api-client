abcf8ba50d9f5d8c88145ec5b949428bcb9d0ef1
Benny Neugebauer <bn@bennyn.de>
2017-05-15 14:25:16 +0200

    Updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,7 @@
-const pkg = require('./package.json');
-const projectName = require('./bower.json').name;
-const webpack = require('webpack');
-
 module.exports = {
-  devtool: 'source-map',
-  entry: {
-    filename: `${__dirname}/dist/commonjs/index.js`
-  },
-  externals: {
-    'axios': true
-  },
+  entry: `${__dirname}/dist/demo.js`,
   output: {
-    filename: `${projectName}.js`,
-    library: 'WireAPIClient',
-    path: `${__dirname}/dist/window`
-  },
-  plugins: [
-    new webpack.BannerPlugin(`${projectName} v${pkg.version}`)
-  ],
-  performance: {
-    maxAssetSize: 100,
-    maxEntrypointSize: 300,
-    hints: 'warning'
-  },
-  resolve: {
-    aliasFields: []
+    path: `${__dirname}/dist`,
+    filename: 'demo-bundle.js'
   }
 };
