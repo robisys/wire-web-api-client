6faa542c1c58178ef0bc41cc004c5520f658b695
Benny Neugebauer <bn@bennyn.de>
2017-05-16 14:53:05 +0200

    readonly

--- src/ts/http/StatusCode.ts
+++ src/ts/http/StatusCode.ts
@@ -1,18 +1,18 @@
 export default class StatusCode {
-  public static ACCEPTED: number = 202;
-  public static BAD_GATEWAY: number = 502;
-  public static BAD_REQUEST: number = 400;
-  public static CONFLICT: number = 409;
-  public static CONNECTIVITY_PROBLEM: number = 0;
-  public static CREATED: number = 201;
-  public static FORBIDDEN: number = 403;
-  public static INTERNAL_SERVER_ERROR: number = 500;
-  public static NO_CONTENT: number = 204;
-  public static NOT_FOUND: number = 404;
-  public static OK: number = 200;
-  public static PRECONDITION_FAILED: number = 412;
-  public static REQUEST_TIMEOUT: number = 408;
-  public static REQUEST_TOO_LARGE: number = 413;
-  public static TOO_MANY_REQUESTS: number = 429;
-  public static UNAUTHORIZED: number = 401;
+  public static readonly ACCEPTED: number = 202;
+  public static readonly BAD_GATEWAY: number = 502;
+  public static readonly BAD_REQUEST: number = 400;
+  public static readonly CONFLICT: number = 409;
+  public static readonly CONNECTIVITY_PROBLEM: number = 0;
+  public static readonly CREATED: number = 201;
+  public static readonly FORBIDDEN: number = 403;
+  public static readonly INTERNAL_SERVER_ERROR: number = 500;
+  public static readonly NO_CONTENT: number = 204;
+  public static readonly NOT_FOUND: number = 404;
+  public static readonly OK: number = 200;
+  public static readonly PRECONDITION_FAILED: number = 412;
+  public static readonly REQUEST_TIMEOUT: number = 408;
+  public static readonly REQUEST_TOO_LARGE: number = 413;
+  public static readonly TOO_MANY_REQUESTS: number = 429;
+  public static readonly UNAUTHORIZED: number = 401;
 }
