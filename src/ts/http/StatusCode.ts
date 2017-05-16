181bbd50114f848fb393cabd131fc4c2c574ffb1
Benny Neugebauer <bn@bennyn.de>
2017-05-16 14:02:12 +0200

    semicolons

--- src/ts/http/StatusCode.ts
+++ src/ts/http/StatusCode.ts
@@ -1,66 +1,66 @@
 export default class StatusCode {
   public static get ACCEPTED(): number {
-    return 202
+    return 202;
   };
 
   public static get BAD_GATEWAY(): number {
-    return 502
+    return 502;
   };
 
   public static get BAD_REQUEST(): number {
-    return 400
+    return 400;
   };
 
   public static get CONFLICT(): number {
-    return 409
+    return 409;
   };
 
   public static get CONNECTIVITY_PROBLEM(): number {
-    return 0
+    return 0;
   };
 
   public static get CREATED(): number {
-    return 201
+    return 201;
   };
 
   public static get FORBIDDEN(): number {
-    return 403
+    return 403;
   };
 
   public static get INTERNAL_SERVER_ERROR(): number {
-    return 500
+    return 500;
   };
 
   public static get NO_CONTENT(): number {
-    return 204
+    return 204;
   };
 
   public static get NOT_FOUND(): number {
-    return 404
+    return 404;
   };
 
   public static get OK(): number {
-    return 200
+    return 200;
   };
 
   public static get PRECONDITION_FAILED(): number {
-    return 412
+    return 412;
   };
 
   public static get REQUEST_TIMEOUT(): number {
-    return 408
+    return 408;
   };
 
   public static get REQUEST_TOO_LARGE(): number {
-    return 413
+    return 413;
   };
 
   public static get TOO_MANY_REQUESTS(): number {
-    return 429
+    return 429;
   };
 
   public static get UNAUTHORIZED(): number {
-    return 401
+    return 401;
   };
 
 }
