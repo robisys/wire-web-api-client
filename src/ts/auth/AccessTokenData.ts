3a23959f03de77f53f787cad7f093553147e4df0
Benny Neugebauer <bn@bennyn.de>
2017-05-16 14:40:06 +0200

    interfaces

--- src/ts/auth/AccessTokenData.ts
+++
@@ -1,9 +0,0 @@
-export default class AccessTokenData {
-  public access_token: string;
-  public expires_in: number;
-  public token_type: string;
-
-  constructor(data: any) {
-    Object.assign(this, data);
-  }
-}
