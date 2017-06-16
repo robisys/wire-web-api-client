52573e58633d183dac815c60dd8980aa79f1d460
Benny Neugebauer <bn@bennyn.de>
2017-06-16 23:06:58 +0200

    Testing reference path comments

--- src/ts/auth/AuthAPI.ts
+++ src/ts/auth/AuthAPI.ts
@@ -1,3 +1,5 @@
+///<reference path="LoginData.ts"/>
+///<reference path="AccessTokenData.ts"/>
 import {AxiosPromise, AxiosRequestConfig, AxiosResponse} from 'axios';
 
 import HttpClient from '../http/HttpClient';
