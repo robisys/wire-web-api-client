d3119ec2d388d4eecea68f1f455f4ff14bb457db
Benny Neugebauer <bn@bennyn.de>
2017-06-18 19:46:51 +0200

    TeamData

--- src/ts/team/TeamAPI.ts
+++ src/ts/team/TeamAPI.ts
@@ -1,10 +1,10 @@
-/// <reference path="TeamChunkData.ts"/>
-/// <reference path="TeamData.ts"/>
 import {AxiosPromise, AxiosRequestConfig, AxiosResponse} from 'axios';
 
 import HttpClient from '../http/HttpClient';
 import MemberData from './MemberData';
 import NewTeamData from './NewTeamData';
+import TeamChunkData from './TeamChunkData';
+import TeamData from './TeamData';
 
 export default class TeamAPI {
   constructor(private client: HttpClient) {}
--- src/ts/team/TeamData.ts
+++ src/ts/team/TeamData.ts
@@ -4,3 +4,5 @@ interface TeamData {
   icon: string;
   creator: string;
 }
+
+export default TeamData;
