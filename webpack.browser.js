217d144fc78d58599eaee9469e46f33a5c31fd9e
Benny Neugebauer <bn@bennyn.de>
2017-08-02 10:50:05 +0200

    feat: Build for Browsers (#127)

---
+++ webpack.browser.js
@@ -0,0 +1,25 @@
+const pkg = require('./package.json');
+const webpack = require('webpack');
+
+let repositoryName = pkg.repository.url.substr(pkg.repository.url.lastIndexOf('/') + 1);
+repositoryName = repositoryName.substr(0, repositoryName.indexOf('.git'));
+
+const camelCasedRepositoryName = repositoryName.replace(/-([a-z])/g, (glob) => glob[1].toUpperCase());
+
+module.exports = {
+  entry: {
+    client: `${__dirname}/dist/commonjs/Client.js`
+  },
+  externals: {
+    'fs-extra': '{}',
+  },
+  output: {
+    filename: `${repositoryName}.js`,
+    library: `${camelCasedRepositoryName}`,
+    path: `${__dirname}/dist`,
+  },
+  plugins: [
+    new webpack.BannerPlugin(`${pkg.name} v${pkg.version}`)
+  ],
+  target: 'web'
+};
