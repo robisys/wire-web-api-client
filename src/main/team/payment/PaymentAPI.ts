733c7e564ae46f160a7d34f933edb50d408341e9
Michael Koppen <michael.koppen@googlemail.com>
2017-09-18 17:14:42 +0200

    PR Review

--- src/main/team/payment/PaymentAPI.ts
+++ src/main/team/payment/PaymentAPI.ts
@@ -1,3 +1,22 @@
+/*
+ * Wire
+ * Copyright (C) 2017 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
 import {AxiosPromise, AxiosRequestConfig, AxiosResponse} from 'axios';
 
 import {PaymentData, PaymentDataUpdate, PaymentStripeInvoice, PaymentStripePlan} from '.';
@@ -34,7 +53,7 @@ export default class TeamAPI {
     return this.client.sendJSON(config).then((response: AxiosResponse) => response.data);
   }
 
-  public getAvailablePlans(teamId: string): Promise<PaymentStripePlan[]> {
+  public getPlans(teamId: string): Promise<PaymentStripePlan[]> {
     const config: AxiosRequestConfig = {
       method: 'get',
       url: `${TeamAPI.URL.TEAMS}/${teamId}/${TeamAPI.URL.BILLING}/${TeamAPI.URL.PLANS}`,
